45b5f9c6f392640e897c66438be664c1bf667128
Sp4Rx <heysuvajit@gmail.com>
2022-06-05 20:58:13 +0530

    Added animations to splash logo

--- lib/ui/screens/splash.dart
+++ lib/ui/screens/splash.dart
@@ -2,8 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:tranzact/ui/commons/atoms/tranzact_logo_big.dart';
 import 'package:tranzact/ui/commons/atoms/wave_container.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> with SingleTickerProviderStateMixin {
+  static const duration = 1;
+  bool startAnimating = false;
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: duration),
+    vsync: this,
+  );
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeIn,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback(_afterLayout);
+    _controller.forward();
+    _controller.addStatusListener((AnimationStatus status) {
+      if (status == AnimationStatus.completed) {
+        //Goto next screen
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _afterLayout(Duration timeStamp) {
+    //Should have been done by bloc
+    //Using setSate to make it faster
+    setState(() {
+      startAnimating = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,12 +56,17 @@ class Splash extends StatelessWidget {
             alignment: Alignment.bottomCenter,
             child: WaveContainer(
               width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width / 2,
+              height: MediaQuery.of(context).size.height / 4,
             ),
           ),
-          const Align(
-            alignment: Alignment.center,
-            child: TranzactLogoBig(),
+          AnimatedPositioned(
+            duration: const Duration(seconds: duration),
+            left: 0,
+            right: 0,
+            top: startAnimating ? 0 : MediaQuery.of(context).size.height / 4,
+            bottom: 0,
+            child: FadeTransition(
+                opacity: _animation, child: const TranzactLogoBig()),
           )
         ],
       ),
